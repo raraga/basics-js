14de9625868bc67b609aef7813bf39468ee6c0ee
Renato Araga <admin@bkprecisioninstruments.com>
2018-12-21 08:34:34 -0800

    Added stuff on appendChild

--- app.js
+++ app.js
@@ -6,6 +6,27 @@
 // A full list of DOM elements can be found here: https://developer.mozilla.org/en-US/docs/Web/Events
 // .innerHTML - changes the content of an HTML element
 
-document.getElementById("stuff").addEventListener("click", function(){
-    document.getElementById("message").innerHTML = "What's up guys!";
-});
+// An example of a simple message
+//document.getElementById("stuff").addEventListener("click", function(){
+//    document.getElementById("message").innerHTML = "What's up guys!";
+//});
+
+// How about with an HTML structure?
+// document.getElementById("stuff").addEventListener("click", function(){
+//     document.getElementById("message").innerHTML = "<button>New Button</button>";
+// });
+
+// However instead of getElement we can create it before hand.
+var x = document.createElement("button");
+// We can give it attributes like id with .setAttribute().
+x.setAttribute("id", "hello");
+// View it in the body with appendChild().
+document.body.appendChild(x);
+
+// Can we write the button's text dynamically?
+y = document.getElementById("hello");
+y.innerHTML = 'Happy Holidays!';
+
+
+
+
